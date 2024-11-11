721e95a6a55db0e0090890d1e6063bc9c9fef02b
Kaleidoscopee <romanj1997@gmail.com>
2024-11-11 17:57:25 -0500

    added the map and key to loop through titles representing the todo items

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,31 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const todoList = [
+  {
+    id: 1,
+    title: "eat"
+  },
+  {
+    id: 2,
+    title: "sleep"
+  },
+  {
+    id: 3,
+    title: "study"
+  },
+];
+
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <h1>Todo List</h1>
       <ul>
-        <li>eat</li>
-        <li>sleep</li>
-        <li>study</li>
+        {todoList.map(function(item){
+          return <li>{item.title}</li>
+        })}
       </ul>
     </>
   )
